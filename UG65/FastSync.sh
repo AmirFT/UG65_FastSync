6cb76859f242c9429e5a6785e4e2fd50502d60ea
Am1rFT <itanhant18@gmail.com>
2022-01-31 22:41:35 +0330

    reverted

--- UG65/FastSync.sh
+++ UG65/FastSync.sh
@@ -9,19 +9,7 @@ echo '**********************************************************************' &&
 echo '**********************************************************************' &&
 echo '**********************************************************************' &&
 echo 'Always use the latest version of this Script'
-echo '**********************************************************************' &&
-echo '**********************************************************************' &&
 
 echo '****************** Enter the ssh password of milesight to login ******************' &&
-
-if [ -z "$1" ]; then
-    echo "IP not supplied."
-    exit 0
-fi
-
-if [ -z "$2" ]; then
-    ssh root@$1 < heliumFastSync.sh
-else
-    ssh root@$1 -p $2 < heliumFastSync.sh
-fi
+ssh root@$1 < heliumFastSync.sh
 
