2872c6eeee876034c745509fb5c19e0c8f1abb14
Am1rFT <itanhant18@gmail.com>
2022-02-05 13:44:59 +0330

    be able to fast sync with dynamic port

--- UG65/FastSync.sh
+++ UG65/FastSync.sh
@@ -9,7 +9,19 @@ echo '**********************************************************************' &&
 echo '**********************************************************************' &&
 echo '**********************************************************************' &&
 echo 'Always use the latest version of this Script'
+echo '**********************************************************************' &&
+echo '**********************************************************************' &&
 
 echo '****************** Enter the ssh password of milesight to login ******************' &&
-ssh root@$1 < heliumFastSync.sh
+
+if [ -z "$1" ]; then
+    echo "IP not supplied."
+    exit 0
+fi
+
+if [ -z "$2" ]; then
+    ssh root@$1 < heliumFastSync.sh
+else
+    ssh root@$1 -p $2 < heliumFastSync.sh
+fi
 
